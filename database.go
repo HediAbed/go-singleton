4d47b45dbd0a2a53abce7adf50ddbf43bf842660
Hedi Abed <hedi.abed.0@gmail.com>
2023-01-09 06:09:55 +0100

    Rename the struct from Database to DatabaseConnection

--- database.go
+++ database.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 )
 
-// Database is the struct we want to make a singleton
-type Database struct {
+// DatabaseConnection is the struct we want to make a singleton
+type DatabaseConnection struct {
 	serverip string
 	serverport int
 }
 
-// Connect using the db instance
-func (db *Database) Connect() {
-	fmt.Println(db.serverip, ":", db.serverport)
+// Connect using the db connection instance
+func (dbc *DatabaseConnection) Connect() {
+	fmt.Println(dbc.serverip, ":", dbc.serverport)
 }
 
-// SetServer sets the ip and port of the db instance
-func (db *Database) SetServer(ip string, port int) {
-	db.serverip = ip
-	db.serverport = port
+// SetServer sets the ip and port of the db connection instance
+func (dbc *DatabaseConnection) SetServer(ip string, port int) {
+	dbc.serverip = ip
+	dbc.serverport = port
 }
 
-// The db instance
-var dbinstance *Database
+// The db connection instance
+var dbConnection *DatabaseConnection
 
-// Database class instance
-func getDatabaseInstance() *Database {
-	return dbinstance
+
+func getDatabaseConnectionInstance() *DatabaseConnection {
+	return dbConnection
 }
