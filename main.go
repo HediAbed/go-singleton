2c019830d8895475f0c936b8791c16d1e5ff8d88
Hedi Abed <hedi.abed.0@gmail.com>
2023-01-09 06:14:14 +0100

    Rename the struct from Database to DatabaseConnection

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 func main() {
-	db := getDatabaseInstance()
+	db := getDatabaseConnectionInstance()
 
 	db.SetServer("197.61.161.74",8080)
 	db.Connect()
 
-	db = getDatabaseInstance()
+	db = getDatabaseConnectionInstance()
 
 	db.SetServer("201.246.87.203",8090)
 	db.Connect()
 
-	db = getDatabaseInstance()
+	db = getDatabaseConnectionInstance()
 
 	db.SetServer("125.98.112.240",9000)
 	db.Connect()
